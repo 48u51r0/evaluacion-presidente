b3acb06da3e4803db602728480ce95377cf8b316
mapellic-siro <mnpellicer@gmail.com>
2020-11-07 18:00:51 +0100

    salidad regresion modelos ideologia y plot coeficientes

--- Scripts/Influye la ideología.R
+++ Scripts/Influye la ideología.R
@@ -109,69 +109,107 @@ reetiquetar <- . %>%
 
 m_voto <- m_voto %>% 
   # Añadimos un identificador de modelo
-  mutate(model = "Recuerdo voto") %>% 
+  mutate(model = 1) %>% 
   # reconvertimos en numerica las vars que rbind ha hecho char
   mutate_at(2:5, as.numeric) %>% 
   reetiquetar()
 
 m_ideologia <- m_ideologia %>% 
-  mutate(model = "Ubicación D-I") %>% 
+  mutate(model = 2) %>% 
   mutate_at(2:5, as.numeric) %>% 
   reetiquetar()
 
 m_adicion <- m_adicion %>% 
-  mutate(model = "Aditivo") %>% 
+  mutate(model = 3) %>% 
   mutate_at(2:5, as.numeric) %>% 
   reetiquetar()
 
 m_interaccion <- m_interaccion %>% 
-  mutate(model = "Interacción") %>% 
+  mutate(model = 4) %>% 
   mutate_at(2:5, as.numeric) %>% 
   reetiquetar()
 
-m <- rbind(m_voto, m_ideologia, m_adicion, m_interaccion)
+m <- rbind(m_voto, m_ideologia, m_adicion, m_interaccion) %>% 
+  arrange(desc(model)) %>% 
+  mutate(model = factor(model,
+                        labels = c("Voto",
+                                   "Ubicación Izq-Dcha",
+                                   "Efecto Aditivo",
+                                   "Efecto Interacción")))
 
 # Ploteamos el dots and whiskers-----------------------------------------------
 # https://mran.microsoft.com/snapshot/2015-08-06/web/packages/dotwhisker/
 # vignettes/dwplot-vignette.html 
-dwplot(m, dodge_size = .5,
+
+dwplot(m, 
+       dodge_size = .7,
        vline = geom_vline(xintercept = 0, 
                           colour = "grey60", 
                           linetype = 2),
-       dot_args = list(aes(shape = model,color=model)),
+       dot_args = list(size = 2,aes(shape = model,color=model)),
        whisker_args = list(aes(color = model)))+
-  scale_color_grey(end = .6)+
+  scale_shape_manual(values = c(18,17,16, 15))+
+  scale_color_grey(start = 0, end = .8)+
   theme_bw() + 
   xlab("Estimación") + 
   ylab("") +
   labs(color = "Modelo", shape = "Modelo")+
+  # invertimos orden en la leyenda
+  guides(color = guide_legend(reverse = TRUE),
+         shape = guide_legend(reverse = TRUE))+
   theme(legend.background = element_rect(colour="grey80"),
         legend.title = element_text(face = "bold"),
         panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank())  
+        panel.grid.minor.x = element_blank())
 
 # Tabla resumen de los modelos de regresión------------------------------------
 # https://www.jakeruss.com/cheatsheets/stargazer
 # /#robust-standard-errors-replicating-statas-robust-option
 
+# adaptamos la salida de stargazer 
+vars_independientes <- c("Intercepto", 
+                         "PP", "PSOE", "Cs", "UP", "MP", "VOX", 
+                         "Ubicación",
+                         "Ubic. * PP", "Ubic. * PSOE", "Ubic. * Cs", 
+                         "Ubic. * UP", "Ubic. * MP", "Ubic. * VOX",
+                         "Hombre", "Nivel Universitario", "Acomodado")
 
+reordenar <- c(1:8, 12:17, 9:11)
 
+modelos_latex <- c("Voto", "Ubicación\\\\&Izq-Dcha", 
+             "Efecto\\\\&Aditivo", "Efecto\\\\&Interacción")
 
-vacio <- coeftest(`Vacío`, vcov. = vcovHC(`Vacío`, type = "HC0"))
-ideologia <- coeftest(Ideología, vcov. = vcovHC(Ideología, type = "HC0"))
-voto <- coeftest(Voto, vcov. = vcovHC(Voto, type = "HC0"))
-completo <- coeftest(Completo, vcov. = vcovHC(Completo, type = "HC0"))
-completo2 <- coeftest(Completo2, vcov. = vcovHC(Completo2, type = "HC0"))
+modelos_html <- c("Voto", "Ubicación <br>Izq-Dcha", 
+                   "Efecto <br>Aditivo", "Efecto <br>Interacción")
 
-#adaptamos la salida de stargazer 
+robust_se <- list(voto = t_voto$std.error, 
+                 ideologia = t_ideologia$std.error, 
+                 adicion = t_adicion$std.error, 
+                 interaccion = t_interaccion$std.error)
+robust_p <- list(voto = t_voto$p.value, 
+                 ideologia = t_ideologia$p.value, 
+                 adicion = t_adicion$p.value, 
+                 interaccion = t_interaccion$p.value)
 
-stargazer(voto, ideologia, adicion, interaccion, type = "text", 
-          intercept.bottom = FALSE, model.names = TRUE,
-          object.names = FALSE, model.numbers = FALSE,
+stargazer(voto, ideologia, adicion, interaccion, 
+          type = "html", 
+          intercept.bottom = FALSE, 
+          model.names = FALSE,
+          object.names = FALSE,
+          column.labels = modelos_html,
+          model.numbers = FALSE,
+          dep.var.labels.include = FALSE,
+          covariate.labels = vars_independientes,
+          order = reordenar,
           star.char = c("*", "**", "***"),
-          star.cutoffs = c(.05, .01, .001))
+          star.cutoffs = c(.05, .01, .001),
+          se = robust_se,
+          p = robust_p,
+          omit.stat = c("f", "ser"),
+          #single.row = TRUE,
+          out = "modelos_alternativos.html")
 
-Completo %>% ggpredict(c("ideol_pers[all]", "RV"), 
+adicion %>% ggpredict(c("ideol_pers[all]", "RV"), 
                        vcov.fun = "vcovHC", 
                        vcov.type = "HC0") %>% 
   filter(!group %in% c("Otros", "MP", "Cs", "Vox")) %>%
